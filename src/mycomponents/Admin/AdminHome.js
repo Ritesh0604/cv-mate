d9c9ffdf17993a4542f1974c296fc867201d9d41
Ishasanghani <102859564+Ishasanghani@users.noreply.github.com>
2023-04-01 02:10:20 +0530

    msg

--- src/mycomponents/Admin/AdminHome.js
+++ src/mycomponents/Admin/AdminHome.js
@@ -4,32 +4,35 @@ import './AdminHome.css';
 
 export default function AdminHome() {
     return (
-        <div className="adminhome-container col-9 px-3">
+        <div className="adminhome-container">
             <div class="d-grid gap-2 d-md-flex justify-content-md-end pt-3">
                 <Link to="http://localhost:3000/adminRegister"><button class="btn btn-danger" type="button" style={{background:"#90EE90"}}>Add</button></Link>
             </div>
+            <div className="table-admin">
             <table className="table">
                 <thead>
                     <tr>
-                        <th scope="col">SNo.</th>
-                        <th scope="col">Faculty Name</th>
-                        <th scope="col">Faculty E-mail</th>
-                        <th scope="col">Domain</th>
-                        <th scope="col">Action</th>
+                        <th scope="col" className=' px-5'>SNo.</th>
+                        <th scope="col " className='px-5'>Faculty Name</th>
+                        <th scope="col " className='px-5'>Faculty E-mail</th>
+                        <th scope="col " className='px-5'>Domain</th>
+                        <th scope="col " className='px-5'>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>jignesh vaniya</td>
-                        <td>jv@gmail.com</td>
-                        <td>Technical</td>
-                        <td><button className="btn btn-sm" style={{background:"red",color:"white"}}>Delete</button></td>
+                        <th scope="row" className='px-5'>1</th>
+                        <td className='px-5'>jignesh vaniya</td>
+                        <td className='px-5'>jv@gmail.com</td>
+                        <td className='px-5'>Technical</td>
+                        <td className='px-5'><button className="btn btn-sm" style={{background:"red",color:"white"}}>Delete</button></td>
                     </tr>
 
                 </tbody>
             </table>
         </div>
+        </div>
+       
 
 
     )
