3827b26f3662b43ef06bd39a9d95f60f1a30530d
shruti <shrutilaheri08@gmail.com>
2023-03-31 14:56:14 +0530

    msg

--- src/mycomponents/Faculty/FacultyDashboard.js
+++ src/mycomponents/Faculty/FacultyDashboard.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Faculty from './Faculty';
 import './FacultyDashboard.css';
+import PendingApproval from './PendingApproval';
 
 export default function FacultyDashboard(){
+    const [state, updateState] = useState("Profile")
     return(
             
     <div className="faculty_d_container">
     <div className="row">
         <h1 className="py-3 text-center text-primary ">VISHWAKARMA GOVERNMENT ENGINEERNING COLLEGE</h1>
     </div>
-    <div className="row">
+    <div className=" options">
         <div className="offcanvas-body col-3 ">
             <nav className="bd-links w-30 navvv" id="bd-docs-nav" aria-label="Docs navigation">
                 <ul className="list-group">
-                    <li className="point left-list list-group-item list-group-item-action ">Profile</li>
-                    <li className="point left-list list-group-item list-group-item-action ">Pending Approval</li>
+                    <li onClick={() => updateState("Profile")} className={"point left-list list-group-item list-group-item-action " + (state==="Profile"&&"active")}>Profile</li>
+                    <li onClick={() => updateState("Pending")} className={"point left-list list-group-item list-group-item-action " + (state==="Pending"&&"active")}>Pending Approval</li>
                     <li className="point left-list list-group-item list-group-item-action ">Approved Approval</li>
                     <li className="point left-list list-group-item list-group-item-action">Log out</li>
                 </ul>
             </nav>
+        </div>
+        <div className='content'>
+        {
+            state==="Profile"
+            &&
+            <Faculty/>
+        }
+        {
+            state==="Pending"
+            &&
+            <PendingApproval/>
+        }
+        
         </div>
     </div>
     </div>
